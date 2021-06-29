f8294649192703c2b0e0456db49d27087ef49fa6
GeekyMonk07 <thelearninggeek07@gmail.com>
2021-06-29 22:58:02 +0530

    MyStyle

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LOVE CALCULATOR',
+      debugShowCheckedModeBanner: false,
+      title: 'Love Calculator',
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
       home: MyCalculator(),
     );
   }
@@ -33,76 +35,84 @@ class _MyCalculatorState extends State<MyCalculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('LOVE CALCULATOR'),
-      ),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Hero(
-                    tag: 'lovegif',
-                    child: Image(image: AssetImage('assets/images/logo.gif')),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue.shade100,
+          title: Text(
+            'LOVE CALCULATOR',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Flexible(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Hero(
+                  tag: 'lovegif',
+                  child: Image(image: AssetImage('assets/images/logo.gif')),
+                ),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  onChanged: (value) {
+                    firstName = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter your name',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
                   ),
-                )),
-            TextField(
-              onChanged: (value) {
-                firstName = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your name',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
                 ),
               ),
-            ),
-            TextField(
-              onChanged: (value) {
-                secondName = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your parterner\'s name',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  onChanged: (value) {
+                    secondName = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter your parterner\'s name',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                loveScore();
-              },
-              child: Text('Calculate'),
-              color: Colors.blue,
-            ),
-          ]),
+              RaisedButton(
+                onPressed: () {
+                  loveScore();
+                },
+                child: Text('Calculate'),
+                color: Colors.lightBlue.shade100,
+              ),
+            ]),
+      ),
     );
   }
 }
@@ -118,20 +128,23 @@ class ScorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-            Flexible(
-        child: Padding(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Flexible(
+            child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Hero(
             tag: 'lovegif',
             child: Image(image: AssetImage('assets/images/logo.gif')),
           ),
         )),
-            Text(
-        'Your Score is : $score',
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-      ),
-          ]),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            'Your Score is : $score',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+          ),
+        ),
+      ]),
     );
   }
 }
